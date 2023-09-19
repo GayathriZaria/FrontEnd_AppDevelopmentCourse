6cfe42acc59ca591627d26bce07e3a0003ee94cf
Gayathri <110535655+GayathriZaria@users.noreply.github.com>
2023-09-19 11:21:35 +0530

    Update Login.jsx

--- day3/src/pages/login/Login.jsx
+++ day3/src/pages/login/Login.jsx
@@ -2,8 +2,21 @@ import {useState} from 'react'
 import toast, {Toaster} from 'react-hot-toast';
 import '../../assets/css/Login.css';
 import { useNavigate } from 'react-router-dom';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleLogin } from '@react-oauth/google';
+
 
 const Login = () => {
+//   <GoogleOAuthProvider clientId="<your_client_id>">...</GoogleOAuthProvider>;
+
+// <GoogleLogin
+//   onSuccess={credentialResponse => {
+//     console.log(credentialResponse);
+//   }}
+//   onError={() => {
+//     console.log('Login Failed');
+//   }}
+// />;
     const nav = useNavigate()
     const [login,setLogin]=useState({
         email:'',
@@ -28,11 +41,22 @@ const Login = () => {
     <div className="main-l d-flex-c-l" >
     <form onSubmit={handleSubmit}>
     <div className="auth-form-l d-flex-c-l">
+    <div className="event-l">
+    <div className='input-field-l'>
+      <h2>EVENT MANAGEMENT</h2>
         <input type="email" name="email" id="email" className='input-field' placeholder='Email' onChange={handleChange} autoFocus required/>
         <input type="password" name="password" id="password" className='input-field' placeholder='Password' onChange={handleChange} required/>
         <button type="submit" className='auth-btn'>Login</button>
+        <h5>Don't have an account ? <span className='text-link-l' onClick={handleRoute}>  Register</span></h5>
     </div>
-    <h5 className='text'>Don't have an account ? <span className='text-link' onClick={handleRoute}>  Register</span></h5>
+        
+        {/* <button class="accountButton" id="GoogleExchange" tabindex="1">
+            ==$0
+            ::before
+            "Google"
+          </button> */}
+    </div>
+    </div>
     </form>
     </div>
     <Toaster
