1e397e3e5cce7d7711add395ab25b448a95105d2
Gayathri <110535655+GayathriZaria@users.noreply.github.com>
2023-10-05 12:17:07 +0530

    Add files via upload

---
+++ day13/src/main/java/com/dynamiccode/event/dto/request/UserRequest.java
@@ -0,0 +1,22 @@
+package com.dynamiccode.event.dto.request;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
+public class UserRequest {
+    private String name;
+    private String email;
+    private String password;
+    private Boolean isEnabled;
+}
---
+++ day13/src/main/java/com/dynamiccode/event/dto/response/UserResponse.java
@@ -0,0 +1,19 @@
+package com.dynamiccode.event.dto.response;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long uid;
+    private String name;
+    private String email;
+    private Boolean isEnabled;
+}
